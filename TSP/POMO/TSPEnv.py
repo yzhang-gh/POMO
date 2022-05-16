3c4de0cf71b0dd86bd0640ac16756c8f9388666b
yzhang <yzhang@d1.com>
2022-05-16 16:21:49 +0800

    TSPEnv.py:load_problems load tsp instances from tensors instead of generating random ones

--- TSP/POMO/TSPEnv.py
+++ TSP/POMO/TSPEnv.py
@@ -1,8 +1,27 @@
-
 from dataclasses import dataclass
 import torch
 
-from TSProblemDef import get_random_problems, augment_xy_data_by_8_fold
+
+def augment_xy_data_by_8_fold(problems):
+    # problems.shape: (batch, problem, 2)
+
+    x = problems[:, :, [0]]
+    y = problems[:, :, [1]]
+    # x,y shape: (batch, problem, 1)
+
+    dat1 = torch.cat((x, y), dim=2)
+    dat2 = torch.cat((1 - x, y), dim=2)
+    dat3 = torch.cat((x, 1 - y), dim=2)
+    dat4 = torch.cat((1 - x, 1 - y), dim=2)
+    dat5 = torch.cat((y, x), dim=2)
+    dat6 = torch.cat((1 - y, x), dim=2)
+    dat7 = torch.cat((y, 1 - x), dim=2)
+    dat8 = torch.cat((1 - y, 1 - x), dim=2)
+
+    aug_problems = torch.cat((dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8), dim=0)
+    # shape: (8*batch, problem, 2)
+
+    return aug_problems
 
 
 @dataclass
@@ -48,11 +67,11 @@ class TSPEnv:
         self.selected_node_list = None
         # shape: (batch, pomo, 0~problem)
 
-    def load_problems(self, batch_size, aug_factor=1):
-        self.batch_size = batch_size
-
-        self.problems = get_random_problems(batch_size, self.problem_size)
+    def load_problems(self, problems, aug_factor=1, device=None):
+        self.batch_size = len(problems)
+        self.problems = problems
         # problems.shape: (batch, problem, 2)
+
         if aug_factor > 1:
             if aug_factor == 8:
                 self.batch_size = self.batch_size * 8
@@ -60,6 +79,9 @@ class TSPEnv:
                 # shape: (8*batch, problem, 2)
             else:
                 raise NotImplementedError
+
+        if device:
+            self.problems = self.problems.to(device)
 
         self.BATCH_IDX = torch.arange(self.batch_size)[:, None].expand(self.batch_size, self.pomo_size)
         self.POMO_IDX = torch.arange(self.pomo_size)[None, :].expand(self.batch_size, self.pomo_size)
@@ -124,4 +146,3 @@ class TSPEnv:
         travel_distances = segment_lengths.sum(2)
         # shape: (batch, pomo)
         return travel_distances
-
